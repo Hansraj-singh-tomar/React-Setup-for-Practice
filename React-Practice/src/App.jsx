7c0b9652c797728bc00f19de57fa801f4a506f99
Hansraj-singh-tomar <hansrajput033@gmail.com>
2023-04-20 18:16:12 +0530

    this is first commit

---
+++ React-Practice/src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+// import Quiz from './Quiz App/Quiz'
+import Lyrics from './Lyrics app/lyrics'
+
+// import ParentComp from './Practice Folder/PracticeFile'
+
+// import MIstake1 from './CommonMistakes/useEffectMistakes/MIstake1'
+// import Mistake2 from './CommonMistakes/useEffectMistakes/Mistake2'
+// import MIstake3 from './CommonMistakes/useEffectMistakes/MIstake3'
+// import Mistake4 from './CommonMistakes/useStateMistakes/Mistake4'
+
+// import Mistake3 from './CommonMistakes/useStateMistakes/Mistake3'
+// import Mistake2 from './CommonMistakes/useStateMistakes/Mistake2'
+// import Mistake1 from './CommonMistakes/useStateMistakes/Mistake1'
+
+// import ClassComp from './ClassComp'
+// import Todo from './Redux/Components/Todo'
+// import Login from './CRUD Operations/Login'
+// import Register from './CRUD Operations/Register'
+
+const App = () => {
+  return (
+    <>
+      {/* <ParentComp /> */}
+      {/* <Todo /> */}
+      {/* <Login /> */}
+      {/* <Register /> */}
+      {/* <ClassComp/> */}
+      {/* <Mistake1/> */}
+      {/* <Mistake2/> */}
+      {/* <Mistake3 /> */}
+      {/* <Mistake4/> */}
+
+      {/* useEffect mistakes */}
+      {/* <MIstake1 /> */}
+      {/* <Mistake2/> */}
+      {/* <MIstake3/> */}
+
+      {/* By GFG - Quiz App */}
+      {/* <Quiz /> */}
+      <Lyrics />
+      
+    </>
+  )
+}
+
+export default App
